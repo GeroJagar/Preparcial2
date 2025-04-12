d2a033d93e40d31926e7c383fcc354d7e747e481
GeroJagar <josegeronimojaramillogarzonj@gmail.com>
2025-04-11 20:46:54 -0500

    Update Punto6.java

--- PreparcialII/src/main/java/Punto6/Punto6.java
+++ PreparcialII/src/main/java/Punto6/Punto6.java
@@ -3,8 +3,25 @@ package Punto6;
 import org.Listas.Conversion;
 import org.Listas.Stack;
 
-public class Punto6 extends Conversion {//La clase conversion tiene los metodos para resolver las notaciones infijas a posfijas y prefijas.
+public class Punto6 extends Conversion {//La clase conversion en el paquete org/Listas tiene los metodos para resolver las notaciones infijas a posfijas y prefijas.
 
+    public static void main(String[] args) {
+        String expresionInfixa = "(A + B) * (C - D)"; // Ejemplo de expresión infija
+        System.out.println("Expresión infija: " + expresionInfixa);
+        
+        // Convertir a notación posfija
+        StringBuilder posfija = posfija(expresionInfixa);
+        System.out.println("Expresión posfija: " + posfija);
+
+        // Convertir a notación prefija
+        StringBuilder prefija = prefija(expresionInfixa);
+        System.out.println("Expresión prefija: " + prefija);
+        
+        // Ejemplo de una expresión en notación polaca (prefija)
+        String expresionPolaca = "+AB-CD*"; // La expresión prefija
+        System.out.println("Resultado de la expresión Polaca: " + resolverPolaca(expresionPolaca));
+    }
+    
     public static int resolverPolaca(String polaca) {
         Stack stack = new Stack();
         for (int i = 0; i < polaca.length(); i++) {
